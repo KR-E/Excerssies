c2eda8c45a48ca2dc25121c666921ff735d95fda
KR-E <kr.elasz@gmail.com>
2018-06-28 21:24:21 +0200

    dodano nowe pliki

--- LotteryDrawing/src/Main.java
+++ LotteryDrawing/src/Main.java
@@ -1,6 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("ile liczb musisz wylosować? ");
+        int k = in.nextInt();
+
+        System.out.println("jaka jest najwieksza liczba? ");
+        int n = in.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = i + 1;
+
+        int[] result = new int[k];
+        for (int i = 0; i < result.length; i++) {
+            int r = (int) (Math.random() * n);
+
+            result[i] = numbers[r];
+
+            numbers[r] = numbers[n - 1];
+            n--;
+        }
+
+        Arrays.sort(result);
+        System.out.println("postaw na nastepujace liczby. dzieki nim zdobredziesz bogactwo");
+        for(int r : result)
+            System.out.println(r);
     }
 }
